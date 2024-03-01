33ad82c23e4dc7ad61c6051e4be4276329d6396e
killbasa <git@shaneb.xyz>
2024-02-29 19:27:11 -0500

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-cloudflare';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+		alias: {
+			$components: resolve('./src/lib/components')
+		}
+	}
+};
+
+export default config;
