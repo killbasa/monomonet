6b194fd5132988a5e4378e58e848fac4b30c652b
killbasa <git@shaneb.xyz>
2024-03-24 18:31:02 -0400

    multiple pages

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,12 @@
-import './styles.scss';
-
 import messages from './assets/messages.json';
 
 const messageString = messages.messages.map((entry) => {
 	return `
-  <li>
-    <p>${entry.author}</p>
-    <p>${entry.message}</p>
-  </li>
-  `;
+	<li>
+		<p>${entry.author}</p>
+		<p>${entry.message}</p>
+	</li>
+  	`;
 });
 
-document.getElementsByTagName('main')[0].innerHTML = `
-<div>
-  <h1>Happy Birthday Mono!</h1>
-  <ul>
-    ${messageString.join('')}
-  </ul>
-</div>
-`;
+document.getElementById('message-list')!.innerHTML = messageString.join('');
