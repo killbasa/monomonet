04b9ab6707adfa5856551e3c17470973f972adf6
killbasa <git@shaneb.xyz>
2024-04-04 17:59:50 -0400

    fix audio?

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -1,6 +1,9 @@
 export const setupSounds = () => {
 	const button = document.getElementById('cibo-audio-button');
-	const sound = document.getElementById('cibo-audio');
+	const sound = document.getElementById('cibo-audio') as HTMLAudioElement;
+
+	const soundURL = new URL('/sounds/yippee.mp3', import.meta.url);
+	sound.src = soundURL.href;
 
 	button?.addEventListener('click', () => {
 		const newSound = sound?.cloneNode() as HTMLAudioElement;
