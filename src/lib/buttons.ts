8a59903149451d0e83121234e869f59c4ac79e1b
killbasa <git@shaneb.xyz>
2024-04-08 21:33:45 -0400

    fix

--- src/lib/buttons.ts
+++ src/lib/buttons.ts
@@ -6,19 +6,21 @@ export async function setupButtonImage(id: string, prefix: string) {
 
 	let imageIndex = 1;
 	let interval = 0;
+
 	const blobs = await loadBlobs([
 		`/ui/${prefix}_button1.png`,
 		`/ui/${prefix}_button2.png`,
 		`/ui/${prefix}_button3.png`
 	]);
 
+	element.src = blobs[0];
+
 	element.onmouseover = () => {
 		element.src = blobs[imageIndex];
 
 		interval = window.setInterval(() => {
-			if (imageIndex < blobs.length - 1) {
-				imageIndex += 1;
-			} else {
+			imageIndex += 1;
+			if (imageIndex > blobs.length - 1) {
 				imageIndex = 1;
 			}
 
@@ -28,6 +30,7 @@ export async function setupButtonImage(id: string, prefix: string) {
 
 	element.onmouseout = () => {
 		element.src = blobs[0];
+		imageIndex = 1;
 		window.clearInterval(interval);
 	};
 }
