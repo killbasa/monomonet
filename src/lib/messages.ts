e1bd7e1798076644b3e715ba2d528b44b372af8b
Kairozen <solasnip-62@hotmail.fr>
2024-04-12 01:30:33 +0200

    custom cursor and few more gifs (#7)
    * sidebar and hydrangea background on titles
    
    * cleanup
    
    * background, font styles and scrolling titles
    
    * cibo background
    
    * custom cursor for website and a few more gifs
    
    ---------
    
    Co-authored-by: GeoffBaud <geoffbaudelet@yahoo.fr>
    Co-authored-by: killbasa <git@shaneb.xyz>

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -4,7 +4,7 @@ export function setupMessages() {
 	const messageString = messages.data.map((entry) => {
 		return `
 		<li class="cibo-message">
-			<img loading="lazy" width="400px" src="${entry.path}" alt="${entry.author}'s cibo"/>
+      <img loading="lazy" width="400px" src="${entry.path}" alt="${entry.author}'s cibo"/>
 			<h2>${entry.author}</h2>
 		</li>
 		  `;
