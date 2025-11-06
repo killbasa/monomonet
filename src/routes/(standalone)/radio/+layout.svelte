addb5ee13d6e477e8ab9b5576ed3f904ec4165bd
killbasa <git@killbasa.com>
2025-11-05 23:22:15 -0500

    fix embed

--- src/routes/(standalone)/radio/+layout.svelte
+++ src/routes/(standalone)/radio/+layout.svelte
@@ -10,10 +10,13 @@
 	<meta property="og:type" content="website" />
 	<meta property="og:url" content="https://monomonet.com/radio.html" />
 	<meta property="og:title" content="Radio 114 FM" />
+	<meta property="og:description" content="Mono Monet's DJ sets" />
 	<meta property="og:image" content="https://monomonet.com/radio/ogimage.webp" />
 
 	<meta name="twitter:title" content="Radio 114 FM" />
+	<meta name="twitter:description" content="Mono Monet's DJ sets" />
 	<meta name="twitter:image" content="https://monomonet.com/radio/ogimage.webp" />
+	<meta name="twitter:card" content="summary_large_image" />
 
 	<meta name="theme-color" content="#FFFE72" />
 
