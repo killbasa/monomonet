e91c0e0547457e47d4ef981841c265e329c70561
killbasa <git@killbasa.com>
2025-11-08 11:51:42 -0500

    drop proxy

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -3,6 +3,6 @@ resource "cloudflare_dns_record" "azura_endpoint" {
   name    = "azura.${data.cloudflare_zone.killbasa_com.name}"
   content = hcloud_server.radio_node.ipv4_address
   type    = "A"
-  ttl     = 1
-  proxied = true
+  ttl     = 300
+  proxied = false
 }
